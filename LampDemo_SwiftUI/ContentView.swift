b3fe2b5817d07fec5a7d2b194429db62f25d2895
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2022-03-21 19:20:44 +0200

    Initial Commit

---
+++ LampDemo_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LampDemo_SwiftUI
+//
+//  Created by Hager Elsayed on 21/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LampDemo_SwiftUI/LampDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampDemo_SwiftUIApp.swift
+//  LampDemo_SwiftUI
+//
+//  Created by Hager Elsayed on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LampDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
